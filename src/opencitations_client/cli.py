03346f92b452da9aef9c7ec1dfb4cb2ce9079c52
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-01-29 21:20:37 +0100

    Build PubMed artifact (#4)

--- src/opencitations_client/cli.py
+++ src/opencitations_client/cli.py
@@ -10,6 +10,10 @@ __all__ = [
 @click.command()
 def main() -> None:
     """CLI for opencitations_client."""
+    from .download import get_pubmed_citations
+
+    edges = get_pubmed_citations(force_process=True)
+    click.echo(f"got {len(edges):,} PubMed-PubMed edges")
 
 
 if __name__ == "__main__":
