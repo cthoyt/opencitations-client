22e3671387b196b2325a7ba395b093701996846a
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-01-29 13:28:59 +0100

    Implement API for metadata (#3)

--- src/opencitations_client/__init__.py
+++ src/opencitations_client/__init__.py
@@ -1,9 +1,27 @@
 """Access and download data from OpenCitations."""
 
-from .api import Citation, get_incoming_citations, get_outgoing_citations
+from .api import (
+    Citation,
+    Metadata,
+    Person,
+    Publisher,
+    Venue,
+    get_articles,
+    get_articles_for_author,
+    get_articles_for_editor,
+    get_incoming_citations,
+    get_outgoing_citations,
+)
 
 __all__ = [
     "Citation",
+    "Metadata",
+    "Person",
+    "Publisher",
+    "Venue",
+    "get_articles",
+    "get_articles_for_author",
+    "get_articles_for_editor",
     "get_incoming_citations",
     "get_outgoing_citations",
 ]
