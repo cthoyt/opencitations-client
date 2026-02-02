e41863277e2aff3e60151a7f432e5d77ce963327
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-02-02 15:52:28 +0100

    Update cache.py

--- src/opencitations_client/cache.py
+++ src/opencitations_client/cache.py
@@ -5,7 +5,7 @@ from functools import lru_cache
 from curies import Reference
 from pystow.graph import GraphCache, GraphCachePaths, build_graph_cache
 
-from .download import MODULE, iter_omid_citations, iter_pubmed_citations
+from .download import MODULE, iter_doi_citations, iter_omid_citations, iter_pubmed_citations
 
 __all__ = [
     "get_incoming_citations",
@@ -40,10 +40,8 @@ def _get_omid_cache() -> GraphCache:
 
 @lru_cache(1)
 def _get_doi_cache() -> GraphCache:
-    if not omid_cache_paths.exists():
-        return build_graph_cache(
-            iter_omid_citations, doi_cache_paths, estimated_edges=1_191_000_000
-        )
+    if not doi_cache_paths.exists():
+        return build_graph_cache(iter_doi_citations, doi_cache_paths, estimated_edges=1_191_000_000)
     return GraphCache(doi_cache_paths)
 
 
@@ -54,16 +52,16 @@ def _get_cache(prefix: str) -> GraphCache:
         case "omid":
             return _get_omid_cache()
         case "doi":
-            return _get_omid_cache()
+            return _get_doi_cache()
         case _:
             raise NotImplementedError(f"citation lookup not implemented for prefix: {prefix}")
 
 
 def get_outgoing_citations(reference: Reference) -> list[str]:
-    """Get outgoing citations as a list of PubMed identifiers."""
+    """Get outgoing citations as a list of local unique identifiers."""
     return _get_cache(reference.prefix).out_edges(reference.identifier)
 
 
 def get_incoming_citations(reference: Reference) -> list[str]:
-    """Get incoming citations as a list of PubMed identifiers."""
+    """Get incoming citations as a list of local unique identifiers."""
     return _get_cache(reference.prefix).in_edges(reference.identifier)
