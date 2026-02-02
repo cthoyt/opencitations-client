bd4cdd2f0513d4ed51b10c7dd52e3e23a9c1c681
Charles Tapley Hoyt <cthoyt@gmail.com>
2026-02-02 15:50:33 +0100

    Update test_cache.py

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -5,9 +5,11 @@ import unittest
 from curies import Reference
 
 from opencitations_client.cache import (
+    _get_omid_cache,
     _get_pubmed_cache,
     get_incoming_citations,
     get_outgoing_citations,
+    omid_cache_paths,
     pubmed_cache_paths,
 )
 
@@ -34,7 +36,7 @@ class TestPubmedGraphCache(unittest.TestCase):
         self.assertGreaterEqual(len(outgoing_citations), 50)  # at least 50
         for c in outgoing_citations:
             incoming = get_incoming_citations(Reference(prefix="pubmed", identifier=c))
-            self.assertIn(example, incoming)
+            self.assertIn(example.identifier, incoming)
 
     def test_missing(self) -> None:
         """Test lookup on a PubMed identifier that isn't indexed by OpenCitations."""
@@ -44,3 +46,28 @@ class TestPubmedGraphCache(unittest.TestCase):
         # see https://pubmed.ncbi.nlm.nih.gov/39514576/
         example = Reference(prefix="pubmed", identifier="39514576")
         self.assertFalse(get_outgoing_citations(example))
+
+
+@unittest.skipUnless(omid_cache_paths.exists(), "can't run tests without cache")
+class TestOMIDGraphCache(unittest.TestCase):
+    """Tests for the OMID graph cache."""
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Set up the test case."""
+        _get_omid_cache()  # warm up the cache, takes 5-10 seconds
+
+    def test_both_ways(self) -> None:
+        """Test lookup on a PubMed identifier that's indexed by OpenCitations."""
+        # A Novel Combination Therapy Using Rosuvastatin And Lactobacillus
+        # Combats Dextran Sodium Sulfate-Induced Colitis In High-Fat Diet-Fed
+        # Rats By Targeting The TXNIP/NLRP3 Interaction And Influencing Gut
+        # Microbiome Composition
+        #
+        # see full metadata at https://api.opencitations.net/meta/v1/metadata/omid:br/06180450607
+        example = Reference(prefix="omid", identifier="br/06180450607")
+        outgoing_citations = get_outgoing_citations(example)
+        self.assertGreaterEqual(len(outgoing_citations), 50)  # at least 50
+        for c in outgoing_citations:
+            incoming = get_incoming_citations(Reference(prefix="omid", identifier=c))
+            self.assertIn(example.identifier, incoming)
